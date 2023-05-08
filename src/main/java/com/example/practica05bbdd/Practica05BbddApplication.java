c10a536ded853dfd25252727793a9700b750a4ab
Danii <danielfernandezlopez75@gmail.com>
2023-05-08 15:48:23 +0200

    Initial commit

---
+++ src/main/java/com/example/practica05bbdd/Practica05BbddApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica05bbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica05BbddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica05BbddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practica05bbdd/Practica05BbddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica05bbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica05BbddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
